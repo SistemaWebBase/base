cda0b4c0f970fd53b271845c91a2ceada9cb6cfa
Thiago Pereira Martinez <thiago.pereira.ti@gmail.com>
2015-09-30 18:34:34 -0400

    Alteracao do titulo do sistema

--- mod/conf/muni/conmunicipios.php
+++ mod/conf/muni/conmunicipios.php
@@ -28,7 +28,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="/">Thiago Sistemas</a>
+					<a class="navbar-brand" href="/">sistema Web</a>
 				</div>
 				<!-- Collect the nav links, forms, and other content for toggling -->
 				<div class="collapse navbar-collapse" id="bs-navbar">
