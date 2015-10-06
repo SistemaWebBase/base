0ac013645dd471004758ccb7269f4ccb4b7e2f78
Raphael Amorim <raphael_amorim@outlook.com>
2015-10-06 23:25:25 +0000

    Criacao Tabela Permisoes --- Falta testar e fazer Ajustes

---
+++ modulos/configuracoes/permissoes/acao.php
@@ -0,0 +1,71 @@
+<?php
+
+   require_once '../../../util/conexao.php';
+   require_once '../../../util/util.php';
+   require_once '../../../util/sessao.php';
+   
+   // validar sessao
+   validarSessao();
+         
+   $id = $_POST['id'];
+   $descricao = tratarTexto($_POST['descricao']);
+   $observacao = tratarTexto($_POST['observacao']);
+   $_action = $_POST['_action'];
+   
+   if ($_action != "exclusao") {
+         // validar campos
+         if (empty($descricao)) {
+	         http_response_code(400);
+        	   echo "Informe a descrição da permissão.";
+	         return;  
+         }
+   
+         if (empty($_action)) {
+	         http_response_code(400);
+	         echo "Falha nos parâmetros da solicitação.";
+               return;
+         }
+   }
+   
+   // Abrir conexao
+   $conexao = new Conexao();
+   
+   // Testar acao
+   $sql = "";
+   
+   if ($_action == "inclusao") {
+         $sql = "insert into permissoes (descricao, observacao) values ('" . $descricao . "', '" . $observacao. "');";
+         $msg1 = "incluir";
+         $msg2 = "inclusão";
+   }
+   
+   if ($_action == "alteracao") {
+         $sql = "update permissoes set descricao='" . $descricao . "',observacao='" . $observacao . "' where id=" . $id;
+         $msg1 = "alterar";
+         $msg2 = "alterado";
+   }
+   
+   if ($_action == "exclusao") {
+         $sql = "delete from permissoes where id=" . $id;
+         $msg1 = "excluir";
+         $msg2 = "excluído";
+   }
+   
+   if (empty($sql)) {
+         http_response_code(400);
+	   echo "Falha nos parâmetros da solicitação. Tente novamente mais tarde ou contate o suporte.";
+         return;
+   }
+   
+   $flag = 0;
+   $result = @pg_query($sql) or $flag = 1;
+   
+   if ($flag == 1) {
+         http_response_code(400);
+         echo "Falha ao " . $msg1 . " registro. Tente novamente mais tarde ou contate o suporte.";
+         return;
+   }
+
+   echo "Registro " . $msg2 . " com sucesso.";
+   
+?>
---
+++ modulos/configuracoes/permissoes/cadastro.php
@@ -0,0 +1,127 @@
+<?php
+        // validar sessao
+        require_once '../../../util/sessao.php';
+
+        validarSessao();
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="format-detection" content="telephone=no">
+		<link rel="shortcut icon" type="image/png" href="/assets/imagens/favicon.png"/>
+		<link rel="apple-touch-icon" type="image/png" href="/assets/imagens/favicon.png"/>
+		<link rel="stylesheet" type="text/css" href="/assets/bootstrap/css/bootstrap.min.css"/>
+		<link rel="stylesheet" type="text/css" href="/assets/css/principal.css" />
+		<link rel="stylesheet" type="text/css" href="assets/css/cadastro.css" />
+		<script type="text/javascript" src="/assets/js/jquery.js"></script>
+		<script type="text/javascript" src="/assets/bootstrap/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="/assets/js/principal.js"></script>
+		<script type="text/javascript" src="assets/js/cadastro.js"></script>
+		<title>SistemaWeb | Thiago Pereira</title> 
+	</head>
+	<body>
+		<?php
+			require_once '../../../util/conexao.php';
+			
+			$_action = "inclusao"; // por padrao, entrar no modo de inclusao
+			
+			// Se passar id, abrir registro
+			$id = $_GET['id'];
+			if (!empty($id)) {
+				// Abrir nova conexão
+				$conexao = new Conexao();
+
+				$sql = "select * from permissoes where id=" . $id;
+				$result = $conexao->query($sql);
+			
+				// Abrir resultado
+				$rows = pg_fetch_all($result);
+			
+				if ($rows == null) {
+					return;
+				}
+			
+				$id = $rows[0]['id'];
+				$descricao = $rows[0]['descricao'];
+				$observacao = $rows[0]['observacao'];
+				$_action = "alteracao";
+			}
+			
+		?>
+		<!-- MENU -->
+		<?php
+		    require_once '../../../util/arquivo.php';
+			
+			import("../../sistema/menu/menu.php");
+		?>
+		<!-- CONTEUDO -->
+		<div class="wrapper" role="main">
+			<div class="container">
+				<div class="row">
+					<!-- SIDEBAR -->
+					<div id="sidebar" class="col-md-3">
+						
+					</div>
+					<!-- AREA DE CONTEUDO -->
+					<div id="conteudo" class="col-xs-12 col-md-9">
+						<!-- FORMULARIO -->
+						<div class="panel panel-primary">
+							<div class="panel-heading">
+								Cadastro de Permissões
+							</div>
+							<div class="panel-body">
+								<form role="form">
+									<div class="form-group col-md-6">
+										<label for="descricao">Descrição: <span class="label label-danger">Obrigatório</span></label>
+										<input type="text" class="form-control" id="descricao" name="descricao" autocomplete="off" maxlength="60" value="<?= $descricao ?>" autofocus>
+									</div>
+									<div class="form-group col-md-6">
+										<label for="observacao">Observação: </label>
+										<input type="text" class="form-control" id="observacao" name="observacao" autocomplete="off" maxlength="60" value="<?= $observacao ?>" >
+									</div>
+									<input type="hidden" name="id" value="<?= $id ?>">
+									<input type="hidden" name="_action" value="<?= $_action ?>">
+								</form>
+							</div>
+						</div>
+						<!-- PAINEL DE AVISO -->
+						<div class="aviso">
+						</div>
+						<!-- PAINEL DE BOTOES -->
+						<div class="btn-control-bar">
+							<div class="panel-heading">
+								<button class="btn btn-success mob-btn-block" onclick="submit('#descricao');">
+									<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+									 Salvar
+								</button>
+								<a href="<?= $_SERVER['HTTP_REFERER'] ?>">
+									<button class="btn btn-warning mob-btn-block">
+										<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+										 Cancelar
+									</button>
+								</a>
+								<button class="btn btn-danger mob-btn-block" style="<?php if ($_action == "inclusao") { echo "display: none"; } ?>" data-toggle="modal" data-target="#modal" onclick="dialogYesNo('esubmit()', null, 'Excluir Permissão', 'Deseja excluir esta permissão ?', 'trash');">
+									<span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+									 Excluir
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- RODAPE -->
+		<footer>
+			<div class="container">
+				<div class="row">
+					
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
