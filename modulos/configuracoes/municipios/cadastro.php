3b2cfd8c7b66264bb0bfb5e8d262de4342ace012
Raphael Amorim <raphael_amorim@outlook.com>
2015-10-08 11:55:54 +0000

    Ajuste - insert

--- modulos/configuracoes/municipios/cadastro.php
+++ modulos/configuracoes/municipios/cadastro.php
@@ -100,11 +100,11 @@
 								<form role="form">
 									<div class="form-group col-md-6">
 										<label for="municipio">Nome do Município: <span class="label label-danger">Obrigatório</span></label>
-										<input type="text" class="form-control" id="municipio" name="municipio" autocomplete="off" maxlength="60" value="<?= $municipio ?>" autofocus <?php permissao(); ?>>
+										<input type="text" class="form-control" id="municipio" name="municipio" autocomplete="off" maxlength="60" value="<?= $municipio ?>" autofocus required <?php permissao(); ?>>
 									</div>
 									<div class="form-group col-md-3">
 										<label for="uf">UF: <span class="label label-danger">Obrigatório</span></label>
-										<select class="form-control" id="uf" name="uf" <?php permissao(); ?>>
+										<select class="form-control" id="uf" name="uf" required <?php permissao(); ?>>
 										<?php
 											$ufs = array('AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RS', 'SC', 'SE', 'SP', 'TO');
 									
@@ -120,7 +120,7 @@
 									</div>
 									<div class="form-group col-md-3">
 										<label for="ibge">IBGE: <span class="label label-danger">Obrigatório</span></label>
-										<input type="number" inputmode="numeric" pattern="[0-9]{5}" class="form-control" id="ibge" name="ibge" autocomplete="off" min="0" max="999999"  value="<?= $ibge ?>" <?php permissao(); ?>>
+										<input type="number" inputmode="numeric" pattern="[0-9]{5}" class="form-control" id="ibge" name="ibge" autocomplete="off" min="0" max="999999"  value="<?= $ibge ?>" required <?php permissao(); ?>>
 									</div>
 									<input type="hidden" name="id" value="<?= $id ?>">
 									<input type="hidden" name="_action" value="<?= $_action ?>">
