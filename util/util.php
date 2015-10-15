64db40c9ae37b4604af97a2a02da6c720f52ccdf
Thiago Pereira Martinez <thiago.pereira.ti@gmail.com>
2015-10-15 16:50:52 -0400

    Ajuste no cadastro de clientes

--- util/util.php
+++ util/util.php
@@ -9,7 +9,11 @@ function tratarTextoSimples($texto) {
 }
 
 function tratarNumero($texto) {
-        return preg_replace("/[^0-9]/", "", trim($texto));
+    return preg_replace("/[^0-9]/", "", trim($texto));
+}
+
+function tratarChave($valor) {
+	return (isset($valor) && ! empty($valor)) ? (int) $valor : "null";
 }
 
 /* validar CPF/CNPJ */
