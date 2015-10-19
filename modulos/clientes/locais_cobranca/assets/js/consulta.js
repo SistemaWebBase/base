1cc085e718a4c2477b774303b1b4d6bd1db3c0c3
Raphael Amorim <raphael_amorim@outlook.com>
2015-10-19 20:49:11 +0000

    Inclusão Tabela Locais de Cobrança (fix #15)

---
+++ modulos/clientes/locais_cobranca/assets/js/consulta.js
@@ -0,0 +1,4 @@
+// Abrir cadastro
+function abrirCadastro(codigo_banco, complemento, token) {
+	window.location.href = "cadastro.php?codigo_banco=" + codigo_banco + "&complemento=" + complemento + "&token=" + token;
+}
